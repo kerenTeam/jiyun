526bad83ecc2211253ea87c0b357842ae197cbf9
youxdd <youxdd0@gmail.com>
2017-09-11 13:55:15 +0800

    no message

--- jiyun.sql
+++ jiyun.sql
@@ -1,41 +1,54 @@
-/*
-Navicat MySQL Data Transfer
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 111.231.193.169 (MySQL 5.6.35-log)
+# Database: cdjiyun6688
+# Generation Time: 2017-09-11 05:36:12 +0000
+# ************************************************************
 
-Source Server         : localhost
-Source Server Version : 50553
-Source Host           : localhost:3306
-Source Database       : jiyun
 
-Target Server Type    : MYSQL
-Target Server Version : 50553
-File Encoding         : 65001
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-Date: 2017-08-18 16:45:15
-*/
 
-SET FOREIGN_KEY_CHECKS=0;
+# Dump of table jy_admin_group
+# ------------------------------------------------------------
 
--- ----------------------------
--- Table structure for jy_admin_group
--- ----------------------------
 DROP TABLE IF EXISTS `jy_admin_group`;
+
 CREATE TABLE `jy_admin_group` (
   `group_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '用户组id',
   `group_name` varchar(10) DEFAULT NULL COMMENT '用户组名',
   `state` int(10) DEFAULT NULL COMMENT '组状态',
   `rules` varchar(5000) DEFAULT NULL COMMENT '规则或权限id',
   PRIMARY KEY (`group_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of jy_admin_group
--- ----------------------------
-INSERT INTO `jy_admin_group` VALUES ('1', '超级管理员', '1', '1');
+LOCK TABLES `jy_admin_group` WRITE;
+/*!40000 ALTER TABLE `jy_admin_group` DISABLE KEYS */;
 
--- ----------------------------
--- Table structure for jy_admin_rule
--- ----------------------------
+INSERT INTO `jy_admin_group` (`group_id`, `group_name`, `state`, `rules`)
+VALUES
+	(1,'超级管理员',1,'1');
+
+/*!40000 ALTER TABLE `jy_admin_group` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table jy_admin_rule
+# ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `jy_admin_rule`;
+
 CREATE TABLE `jy_admin_rule` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '网站规则权限表',
   `pid` int(10) DEFAULT NULL COMMENT '父级id',
@@ -47,16 +60,15 @@ CREATE TABLE `jy_admin_rule` (
   `islink` varchar(100) DEFAULT NULL COMMENT '链接',
   `sort` varchar(10) DEFAULT NULL COMMENT '排序',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of jy_admin_rule
--- ----------------------------
 
--- ----------------------------
--- Table structure for jy_admin_user
--- ----------------------------
+
+# Dump of table jy_admin_user
+# ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `jy_admin_user`;
+
 CREATE TABLE `jy_admin_user` (
   `user_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '后台管理员用户表',
   `group_id` int(10) NOT NULL COMMENT '用户类型  用户组',
@@ -66,18 +78,25 @@ CREATE TABLE `jy_admin_user` (
   `user_state` varchar(255) DEFAULT NULL COMMENT '用户状态',
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`user_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of jy_admin_user
--- ----------------------------
-INSERT INTO `jy_admin_user` VALUES ('1', '1', null, 'admin1', 'e10adc3949ba59abbe56e057f20f883e', '1', '2017-07-25 15:09:53');
-INSERT INTO `jy_admin_user` VALUES ('2', '0', null, 'root', 'e10adc3949ba59abbe56e057f20f883e', null, '2017-07-25 15:09:56');
+LOCK TABLES `jy_admin_user` WRITE;
+/*!40000 ALTER TABLE `jy_admin_user` DISABLE KEYS */;
 
--- ----------------------------
--- Table structure for jy_banner
--- ----------------------------
+INSERT INTO `jy_admin_user` (`user_id`, `group_id`, `head_pic`, `username`, `password`, `user_state`, `create_time`)
+VALUES
+	(1,1,NULL,'admin1','9f0c07088c8228a2f209477783b2a2fa','1','2017-07-25 15:09:53'),
+	(2,0,NULL,'root','e10adc3949ba59abbe56e057f20f883e',NULL,'2017-07-25 15:09:56');
+
+/*!40000 ALTER TABLE `jy_admin_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table jy_banner
+# ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `jy_banner`;
+
 CREATE TABLE `jy_banner` (
   `banner_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'banner表',
   `banner_pic` varchar(500) DEFAULT NULL COMMENT 'banner 图片',
@@ -85,18 +104,24 @@ CREATE TABLE `jy_banner` (
   `banner_url` varchar(200) DEFAULT NULL COMMENT 'banner 链接',
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`banner_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of jy_banner
--- ----------------------------
-INSERT INTO `jy_banner` VALUES ('1', 'upload/banner/2017-07-25_083358.jpg', '1', 'http://www.ba123idu.com', '2017-07-25 16:05:24');
-INSERT INTO `jy_banner` VALUES ('3', 'upload/banner/2017-07-27_182130.png', '1', 'http://www.baidu.com', '2017-07-27 18:21:30');
+LOCK TABLES `jy_banner` WRITE;
+/*!40000 ALTER TABLE `jy_banner` DISABLE KEYS */;
 
--- ----------------------------
--- Table structure for jy_category
--- ----------------------------
+INSERT INTO `jy_banner` (`banner_id`, `banner_pic`, `banner_state`, `banner_url`, `create_time`)
+VALUES
+	(1,'upload/banner/2017-08-29_110526.png','1','http://www.cdjiyun.com/','2017-07-25 16:05:24');
+
+/*!40000 ALTER TABLE `jy_banner` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table jy_category
+# ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `jy_category`;
+
 CREATE TABLE `jy_category` (
   `cate_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '分类表',
   `pid` int(10) DEFAULT '0' COMMENT '父级id',
@@ -110,19 +135,26 @@ CREATE TABLE `jy_category` (
   `sort` int(10) DEFAULT NULL COMMENT '排序',
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`cate_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of jy_category
--- ----------------------------
-INSERT INTO `jy_category` VALUES ('10', '0', '2', '新闻动态', null, '', null, null, null, null, '2017-07-28 15:38:11');
-INSERT INTO `jy_category` VALUES ('11', '0', '2', '产品更新', null, '', null, null, null, null, '2017-07-28 15:38:27');
-INSERT INTO `jy_category` VALUES ('12', '0', '2', '安全咨讯', null, '', null, null, null, null, '2017-07-28 15:38:50');
+LOCK TABLES `jy_category` WRITE;
+/*!40000 ALTER TABLE `jy_category` DISABLE KEYS */;
 
--- ----------------------------
--- Table structure for jy_news
--- ----------------------------
+INSERT INTO `jy_category` (`cate_id`, `pid`, `type`, `cate_name`, `icon`, `info`, `keywords`, `content`, `url`, `sort`, `create_time`)
+VALUES
+	(10,0,2,'新闻动态',NULL,'',NULL,NULL,NULL,NULL,'2017-07-28 15:38:11'),
+	(11,0,2,'产品更新',NULL,'',NULL,NULL,NULL,NULL,'2017-07-28 15:38:27'),
+	(12,0,2,'安全咨讯',NULL,'',NULL,NULL,NULL,NULL,'2017-07-28 15:38:50');
+
+/*!40000 ALTER TABLE `jy_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table jy_news
+# ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `jy_news`;
+
 CREATE TABLE `jy_news` (
   `news_id` int(10) NOT NULL AUTO_INCREMENT COMMENT '新闻资讯表',
   `cate_id` int(10) DEFAULT NULL COMMENT '分类id',
@@ -133,21 +165,28 @@ CREATE TABLE `jy_news` (
   `read` int(10) DEFAULT '0' COMMENT '点击量',
   `state` int(1) DEFAULT '1' COMMENT '状态',
   PRIMARY KEY (`news_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of jy_news
--- ----------------------------
-INSERT INTO `jy_news` VALUES ('1', '6', '1234567', 'upload/news/2017-07-26_184838.jpg', '<p><br></p>', '2017-07-26 18:48:38', '0', '1');
-INSERT INTO `jy_news` VALUES ('2', '1', '34567', 'upload/news/2017-07-26_185028.png', '\"<p>说法打发打发发到公司分管岁的法国</p><img src=\\\"../upload/news/2017-07-27_0941220.png\\\">\"', '2017-07-26 18:50:28', '0', '1');
-INSERT INTO `jy_news` VALUES ('9', '10', '柔柔弱弱', 'upload/news/2017-07-27_095441.png', '<p>尔特瑞特人</p><img src=\"../upload/news/2017-07-27_0954350.png\" class=\"w-e-selected\">', '2017-07-27 09:54:41', '0', '1');
-INSERT INTO `jy_news` VALUES ('6', '10', 'dewer', 'upload/news/2017-07-26_193947.png', '\"<p>sfddfadf</p>\"', '2017-07-26 19:39:47', '0', '1');
-INSERT INTO `jy_news` VALUES ('7', '10', 'sfdfasdfa', 'upload/news/2017-07-26_194022.png', '<p>sdfdfdgfdgdfgdfdf sdfsdf</p><p>sdfsdfsdf</p><p>sdfdsfdf</p><img src=\"../upload/editer/2017-07-28_1727570.png\"><img src=\"../upload/editer/2017-07-28_1727572.png\" class=\"\">', '2017-07-26 19:40:23', '0', '1');
+LOCK TABLES `jy_news` WRITE;
+/*!40000 ALTER TABLE `jy_news` DISABLE KEYS */;
 
--- ----------------------------
--- Table structure for jy_parnter
--- ----------------------------
+INSERT INTO `jy_news` (`news_id`, `cate_id`, `title`, `thumb`, `content`, `create_time`, `read`, `state`)
+VALUES
+	(1,6,'1234567','upload/news/2017-07-26_184838.jpg','<p><br></p>','2017-07-26 18:48:38',0,1),
+	(2,1,'34567','upload/news/2017-07-26_185028.png','\"<p>说法打发打发发到公司分管岁的法国</p><img src=\\\"../upload/news/2017-07-27_0941220.png\\\">\"','2017-07-26 18:50:28',0,1),
+	(9,10,'柔柔弱弱','upload/news/2017-07-27_095441.png','<p>尔特瑞特人</p><img src=\"../upload/news/2017-07-27_0954350.png\" class=\"w-e-selected\">','2017-07-27 09:54:41',0,1),
+	(6,10,'dewer','upload/news/2017-07-26_193947.png','\"<p>sfddfadf</p>\"','2017-07-26 19:39:47',0,1),
+	(7,10,'sfdfasdfa','upload/news/2017-07-26_194022.png','<p>sdfdfdgfdgdfgdfdf sdfsdf</p><p>sdfsdfsdf</p><p>sdfdsfdf</p><img src=\"../upload/editer/2017-07-28_1727570.png\"><img src=\"../upload/editer/2017-07-28_1727572.png\" class=\"\">','2017-07-26 19:40:23',0,1);
+
+/*!40000 ALTER TABLE `jy_news` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table jy_parnter
+# ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `jy_parnter`;
+
 CREATE TABLE `jy_parnter` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '合作伙伴',
   `name` varchar(200) DEFAULT NULL COMMENT '名称',
@@ -155,17 +194,24 @@ CREATE TABLE `jy_parnter` (
   `logo` varchar(400) DEFAULT NULL COMMENT 'logo图',
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of jy_parnter
--- ----------------------------
-INSERT INTO `jy_parnter` VALUES ('2', '345678', '4567890-', 'upload/parnter/2017-07-27_173605.jpg', '2017-07-27 17:36:05');
+LOCK TABLES `jy_parnter` WRITE;
+/*!40000 ALTER TABLE `jy_parnter` DISABLE KEYS */;
 
--- ----------------------------
--- Table structure for jy_product
--- ----------------------------
+INSERT INTO `jy_parnter` (`id`, `name`, `url`, `logo`, `create_time`)
+VALUES
+	(2,'345678','4567890-','upload/parnter/2017-08-29_110745.png','2017-07-27 17:36:05');
+
+/*!40000 ALTER TABLE `jy_parnter` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table jy_product
+# ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `jy_product`;
+
 CREATE TABLE `jy_product` (
   `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '产品表',
   `title` varchar(200) DEFAULT NULL COMMENT '标题',
@@ -177,17 +223,24 @@ CREATE TABLE `jy_product` (
   `state` int(2) DEFAULT '1' COMMENT '产品状态  1正常   0下架',
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of jy_product
--- ----------------------------
-INSERT INTO `jy_product` VALUES ('2', '这些都是', '分割风格豆腐干个风格豆腐干豆腐干地方感豆腐干豆腐干豆腐干豆腐干豆腐干大概风格豆腐干', '345654', '7', 'upload/product/2017-07-27_142555.jpg', '<p>打发士大夫士大夫</p>', '0', '2017-07-27 14:25:55');
+LOCK TABLES `jy_product` WRITE;
+/*!40000 ALTER TABLE `jy_product` DISABLE KEYS */;
 
--- ----------------------------
--- Table structure for jy_system
--- ----------------------------
+INSERT INTO `jy_product` (`id`, `title`, `info`, `price`, `cate_id`, `thumb`, `content`, `state`, `create_time`)
+VALUES
+	(2,'这些都是','分割风格豆腐干个风格豆腐干豆腐干地方感豆腐干豆腐干豆腐干豆腐干豆腐干大概风格豆腐干',345654,7,'upload/product/2017-07-27_142555.jpg','<p>打发士大夫士大夫</p>',0,'2017-07-27 14:25:55');
+
+/*!40000 ALTER TABLE `jy_product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table jy_system
+# ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `jy_system`;
+
 CREATE TABLE `jy_system` (
   `id` int(1) NOT NULL COMMENT '网站信息',
   `seotitle` varchar(300) DEFAULT NULL COMMENT 'seo',
@@ -196,17 +249,24 @@ CREATE TABLE `jy_system` (
   `logo` varchar(500) DEFAULT NULL COMMENT '网站logo',
   `code` varchar(500) DEFAULT NULL COMMENT '二维码地址',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of jy_system
--- ----------------------------
-INSERT INTO `jy_system` VALUES ('1', '234xcvc', 'dsadasdads', '567', 'upload/banner/2017-07-28_143749.png', 'upload/banner/2017-07-28_1437491.png');
+LOCK TABLES `jy_system` WRITE;
+/*!40000 ALTER TABLE `jy_system` DISABLE KEYS */;
 
--- ----------------------------
--- Table structure for jy_system_log
--- ----------------------------
+INSERT INTO `jy_system` (`id`, `seotitle`, `keywords`, `record`, `logo`, `code`)
+VALUES
+	(1,'极云天下','IDC','   Copyright © 2015-2017 cdjiyun Cloud. All Rights Reserved.极云 版权所有\r\n          ','upload/banner/2017-08-29_110655.png','upload/banner/2017-07-28_1437491.png');
+
+/*!40000 ALTER TABLE `jy_system` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table jy_system_log
+# ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `jy_system_log`;
+
 CREATE TABLE `jy_system_log` (
   `id` int(10) NOT NULL COMMENT '系统日志表',
   `user_id` int(10) NOT NULL COMMENT '操作人',
@@ -214,8 +274,14 @@ CREATE TABLE `jy_system_log` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '操作时间',
   `login_ip` varchar(200) DEFAULT NULL COMMENT '登陆ip',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of jy_system_log
--- ----------------------------
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
